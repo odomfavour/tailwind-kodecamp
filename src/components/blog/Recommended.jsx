555714e05d77f8d2e8ee324e17bdc5f19c559077
oladokunlt <lukmanoladokun86@gmail.com>
2023-09-27 00:51:01 +0100

    ft: Added blog page for mobile and desktop

---
+++ src/components/blog/Recommended.jsx
@@ -0,0 +1,110 @@
+// import React from "react";
+import { Link } from "react-router-dom";
+import feauredImage from "../../assets/images/image-blog-card.png";
+
+const Recommended = () => {
+  return (
+    <div className="latest-wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[60px]">
+      <figure className="w-full md:w-80 bg-[#D4D4D4] hover:bg-white transition-all rounded-[10px]">
+        <div className="w-full h-80 rounded-t-[10px] rounded-tr-[10px] mb-4 overflow-y-hidden">
+          <img
+            src={feauredImage}
+            alt="featured image"
+            className=" w-full rounded-t-[10px] rounded-tr-[10px]"
+          />
+        </div>
+        <div className="px-4 ">
+          <figcaption className="text-[#AAAAAA] font-bold text-[14px] leading-[16.8px]">
+            <p className="flex gap-4 mb-2">
+              <time dateTime="2022-06-22"> 22-06-2022 </time> <span> | </span>
+              <span>Anthony John</span>
+            </p>
+          </figcaption>
+          <article>
+            <p className="text-primaryBlue font-bold mb-4 ">Business</p>
+            <h4 className="font-bold leading-[19.2px] mb-1">
+              Retracing Our Steps in the Desisgn Industry
+            </h4>
+            <p className="text-[14px] leading-6 mb-4 ">
+              {" "}
+              Our mission is to admit nuture and deploy next generation ...{" "}
+            </p>
+            <div className="flex justify-end mb-[39px] ">
+              <Link to="#" className="font-bold text-primaryBlue">
+                Read
+              </Link>
+            </div>
+          </article>
+        </div>
+      </figure>
+
+      <figure className="w-full md:w-80 bg-[#D4D4D4] hover:bg-white transition-all rounded-[10px]">
+        <div className="w-full h-80 rounded-t-[10px] rounded-tr-[10px] mb-4 overflow-y-hidden">
+          <img
+            src={feauredImage}
+            alt="featured image"
+            className=" w-full rounded-t-[10px] rounded-tr-[10px]"
+          />
+        </div>
+        <div className="px-4 ">
+          <figcaption className="text-[#AAAAAA] font-bold text-[14px] leading-[16.8px]">
+            <p className="flex gap-4 mb-2">
+              <time dateTime="2022-06-22"> 22-06-2022 </time> <span> | </span>
+              <span>Anthony John</span>
+            </p>
+          </figcaption>
+          <article>
+            <p className="text-primaryBlue font-bold mb-4 ">Business</p>
+            <h4 className="font-bold leading-[19.2px] mb-1">
+              Retracing Our Steps in the Desisgn Industry
+            </h4>
+            <p className="text-[14px] leading-6 mb-4 ">
+              {" "}
+              Our mission is to admit nuture and deploy next generation ...{" "}
+            </p>
+            <div className="flex justify-end mb-[39px] ">
+              <Link to="#" className="font-bold text-primaryBlue">
+                Read
+              </Link>
+            </div>
+          </article>
+        </div>
+      </figure>
+
+      <figure className="w-full md:w-80 bg-[#D4D4D4] hover:bg-white transition-all rounded-[10px]">
+        <div className="w-full h-80 rounded-t-[10px] rounded-tr-[10px] mb-4 overflow-y-hidden">
+          <img
+            src={feauredImage}
+            alt="featured image"
+            className=" w-full rounded-t-[10px] rounded-tr-[10px]"
+          />
+        </div>
+        <div className="px-4 ">
+          <figcaption className="text-[#AAAAAA] font-bold text-[14px] leading-[16.8px]">
+            <p className="flex gap-4 mb-2">
+              <time dateTime="2022-06-22"> 22-06-2022 </time> <span> | </span>
+              <span>Anthony John</span>
+            </p>
+          </figcaption>
+          <article>
+            <p className="text-primaryBlue font-bold mb-4 ">Business</p>
+            <h4 className="font-bold leading-[19.2px] mb-1">
+              Retracing Our Steps in the Desisgn Industry
+            </h4>
+            <p className="text-[14px] leading-6 mb-4 ">
+              {" "}
+              Our mission is to admit nuture and deploy next generation ...{" "}
+            </p>
+            <div className="flex justify-end mb-[39px] ">
+              <Link to="#" className="font-bold text-primaryBlue">
+                Read
+              </Link>
+            </div>
+          </article>
+        </div>
+      </figure>
+    </div>
+  );
+};
+
+export default Recommended;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,22 +1,45 @@
 // import React from 'react';
 import { BiSearch } from "react-icons/bi";
 import MainLayout from "../components/layout/MainLayout";
+// import heroBlogDesktop from "../assets/images/hero-blog-desktop.png";
+import heroBlogMobile from "../assets/images/hero-blog-mobile.png";
+import LatestUpdates from "../components/blog/LatestUpdates";
+import Recommended from "../components/blog/Recommended";
+import Categories from "../components/blog/Categories";
 
 const Blog = () => {
   return (
     <MainLayout>
       <div className="main">
-        <div className="w-11/12 mx-auto  border bg-blue-100">
-          <div className="latestupdate-searchblog h-[36px] md:h-[56px] flex md:justify-between ">
+        {/* Hero section */}
+        <section
+          className="hero-blog h-96 md:h-[296px] opacity-90 bg-[#2B2B2B] bg-cover bg-center md:bg-none lg:bg-[url(`../assets/images/hero-blog-desktop.png`)] mb-[52px] md:mb-[112px] "
+          style={{
+            backgroundImage: `url(${heroBlogMobile})`,
+          }}
+        >
+          <div className="h-full opacity-90 bg-[#2B2B2B] text-white grid place-items-center">
+            <h1 className="text-[28px] md:text-[56px] font-bold leading-[33.6px] md:leading-[64px] mt-[-120px] md:mt-0">
+              Our Blog
+            </h1>
+          </div>
+        </section>
+
+        {/* Latest posts section */}
+        <section className="w-11/12 mx-auto mb-[60px] ">
+          <div className="latestupdate-searchblog h-[36px] md:h-[56px] flex justify-between mb-[60px] ">
             <div className="latest-update flex gap-[10px] md:gap-4 items-center">
               <div className="h-[17px] md:h-[32px] w-1 md:w-[10px] bg-primaryBlue"></div>
               <h2 className="text-2xl md:text-[40px] leading-[28.8px] md:leading-[48px] font-bold color-[#2B2B2B]">
                 Latest Updates
               </h2>
             </div>
-            <form action="" className=" absolute top-0 md:static  ">
+            <form
+              action=""
+              className=" absolute top-[300px] right-6 left-6  md:static  "
+            >
               <div className="flex items-center gap-3 w-full">
-                <div className="w-5/6 lg:w-[403px] ">
+                <div className="w-full md:w-5/6 lg:w-[403px] ">
                   <input
                     type="text"
                     className="border py-[13px] px-[21px] h-[56px] outline-none w-full bg-[#D4E4FB] border-l-4 rounded-[5px] border-l-primaryBlue"
@@ -31,7 +54,38 @@ const Blog = () => {
               </div>
             </form>
           </div>
-        </div>
+          <LatestUpdates />
+        </section>
+
+        {/* Recomended posts section */}
+        <section className="bg-[#D4E4FB] pt-8 pb-7 mb-[60px]">
+          <div className="w-11/12 mx-auto  ">
+            <div className="latestupdate-searchblog h-[36px] md:h-[56px] flex justify-left mb-[60px] ">
+              <div className="latest-update flex gap-[10px] md:gap-4 items-center">
+                <div className="h-[17px] md:h-[32px] w-1 md:w-[10px] bg-primaryBlue"></div>
+                <h2 className="text-2xl md:text-[40px] leading-[28.8px] md:leading-[48px] font-bold color-[#2B2B2B]">
+                  Recommended posts
+                </h2>
+              </div>
+            </div>
+            <Recommended />
+          </div>
+        </section>
+
+        {/* categories  */}
+        <section>
+          <div className="w-11/12 mx-auto pb-8">
+            <div className="h-[36px] md:h-[56px] flex justify-left mb-[60px] ">
+              <div className="latest-update flex gap-[10px] md:gap-4 items-center">
+                <div className="h-[17px] md:h-[32px] w-1 md:w-[10px] bg-primaryBlue"></div>
+                <h2 className="text-2xl md:text-[40px] leading-[28.8px] md:leading-[48px] font-bold color-[#2B2B2B]">
+                  Categories
+                </h2>
+              </div>
+            </div>
+            <Categories />
+          </div>
+        </section>
       </div>
     </MainLayout>
   );
