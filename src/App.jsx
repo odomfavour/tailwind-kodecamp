fed0586f7d2cd8059264552884083f58e755e1d1
Igahsamuel <igahsamuela2@gmail.com>
2023-09-28 17:20:49 +0100

    resolved

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/contact" element={<Contact />} />
       <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 }
