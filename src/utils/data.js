b557a70a1777a5504b42efc881b9c95e2f20d7d0
fiiwebarry <fiiwebarry@yahoo.com>
2023-09-29 23:39:39 +0100

    update-on-branch

--- src/utils/data.js
+++ src/utils/data.js
@@ -12,3 +12,19 @@ export const loggedIn = [
   { id: 3, name: 'Profile', url: '/profile' },
   { id: 4, name: 'Logout', url: '/logout' },
 ];
+
+export const devTeam =[
+  
+  
+  {id:1},
+  {id:2},
+  {id:3},
+  {id:4},
+  {id:5},
+  {id:6},
+  {id:7},
+  {id:8},
+  {id:9},
+  {id:10},
+
+]
