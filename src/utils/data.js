0060f7a5926d589be8938b17cdb87178e008f8e4
Purpose11 <adebayofatai924@gmail.com>
2023-10-01 22:04:58 +0100

    first commit

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,14 +1,29 @@
 export const navLinks = [
-  { id: 1, name: 'Home', url: '/' },
-  { id: 2, name: 'Recruits', url: '/recruits' },
-  { id: 3, name: 'About Us', url: '/about' },
-  { id: 4, name: 'Contact', url: '/contact' },
-  { id: 5, name: 'Blog', url: '/blog' },
+  { id: 1, name: "Home", url: "/" },
+  { id: 2, name: "Recruits", url: "/recruits" },
+  { id: 3, name: "About Us", url: "/about" },
+  { id: 4, name: "Contact", url: "/contact" },
+  { id: 5, name: "Blog", url: "/blog" },
 ];
 
 export const loggedIn = [
-  { id: 1, name: 'Message', url: '/' },
-  { id: 2, name: 'Notification', url: '/notification' },
-  { id: 3, name: 'Profile', url: '/profile' },
-  { id: 4, name: 'Logout', url: '/logout' },
+  { id: 1, name: "Message", url: "/" },
+  { id: 2, name: "Notification", url: "/notification" },
+  { id: 3, name: "Profile", url: "/profile" },
+  { id: 4, name: "Logout", url: "/logout" },
+];
+
+export const tabTitles = [
+  {
+    id: 1,
+    title: "Personal Details",
+  },
+  {
+    id: 2,
+    title: "Experience",
+  },
+  {
+    id: 3,
+    title: "Personal Reviews",
+  },
 ];
