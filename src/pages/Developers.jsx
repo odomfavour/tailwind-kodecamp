83f27f0e8db2765b6b935f2bd82553b915d47472
fiiwebarry <fiiwebarry@yahoo.com>
2023-10-01 21:41:47 +0100

    update

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -19,7 +19,7 @@ const Developers = () => {
 
             <div className=" w-[70%] container mx-auto">
                 <button className="w-[178px] h-[48px] text-[#FFFFFF] text-[16px] font-bold  bg-[#2776EA] mt-[90px] rounded-t-md">Find a Candidate</button>
-                <div className="md:w-[1083px] md:h-[88px] bg-[#F5F5F5] p-4  md:flex gap-11 ">
+                <div className="bg-[#F5F5F5] p-4  flex flex-col md:flex-row  flex-wrap  items-center   gap-5 md:gap-11 ">
 
                     <div className="relative">
                         <div className="absolute top-3">
@@ -55,17 +55,17 @@ const Developers = () => {
                     </div>
 
 
-                    <button className="bg-[#2776EA] w-[210px] h-[46px] ml-[60px]  text-[#FFFFFF]">Search</button>
+                    <button className="bg-[#2776EA] w-[210px] h-[46px]   md:ml-[60px]  text-[#FFFFFF]">Search</button>
 
                 </div>
 
             </div>
             <div className="flex w-[80%] container mx-auto mt-7 gap-4">
                 <img className="w-[10px] h-[48px] mt-3" src={rect} />
-                <h2 className="text-[48px] font-bold">Software  Developers</h2>
+                <h2 className=" text-[24px] md:text-[48px] mt-4 md:mt-0 font-bold">Software  Developers</h2>
             </div>
 
-            <div className="flex flex-wrap gap-[170px] w-[70%] container mx-auto mt-9">
+            <div className="flex flex-wrap  justify-between gap-y-11 lg:gap-[170px] lg:w-[70%] w-[80%] container mx-auto mt-9">
                 {devTeam.map((developers) => {
 
                     const { id, Image, NAME, experience, LOCATION } = developers
@@ -104,7 +104,7 @@ const Developers = () => {
                 })}
             </div>
 
-            <button className="w-[227px] h-[64px] px-12 py-5 border-[#2776EA] border-2 flex m-auto mt-[90px] mb-7">See More</button>
+            <button className="w-[227px] h-[64px]  py-5 justify-center border-[#2776EA] border-2 flex m-auto mt-[90px] mb-7">See More</button>
 
 
         </section>
