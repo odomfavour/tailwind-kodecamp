2636ac9e78ace4658bf05207c72413802f764e4b
odomfavour <ogochukwuodom@gmail.com>
2023-10-09 12:13:00 +0100

    ch: added fix for conflict

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,50 +1,49 @@
 /* eslint-disable no-unused-vars */
-import ellipse1 from '../assets/images/Ellipse 41.svg'
-import ellipse2 from '../assets/images/Ellipse 50.svg'
-import ellipse3 from '../assets/images/Ellipse 55.svg'
-import ellipse4 from '../assets/images/Ellipse 67.svg'
-import ellipse5 from '../assets/images/Ellipse 68.svg'
-import ellipse6 from '../assets/images/Ellipse 69.svg'
-import dev from "/src/assets/images/developer.png";
-import dev1 from "/src/assets/images/developer1.png";
-import dev2 from "/src/assets/images/developer2.png";
-import dev3 from "/src/assets/images/developer3.png";
-import dev4 from "/src/assets/images/developer4.png";
-import dev5 from "/src/assets/images/developer5.png";
-import dev6 from "/src/assets/images/developer6.png";
-import dev7 from "/src/assets/images/developer7.png";
-import dev8 from "/src/assets/images/developer8.png";
-import dev9 from "/src/assets/images/developer9.png";
-import dev10 from "/src/assets/images/developer10.png";
-import dev11 from "/src/assets/images/developer11.png";
-import dev12 from "/src/assets/images/developer12.png";
-import dev14 from "/src/assets/images/developery.png";
-import dev15 from "/src/assets/images/developer15.png";
-import dev16 from "/src/assets/images/developer16.png";
-import dev17 from "/src/assets/images/developer17.png";
-import dev18 from "/src/assets/images/developer18.png";
-import dev19 from "/src/assets/images/developer19.png";
-import dev20 from "/src/assets/images/developer20.png";
-import dev21 from "/src/assets/images/developer21.png";
-import dev22 from "/src/assets/images/developer22.png";
-import dev23 from "/src/assets/images/developer23.png";
-import dev24 from "/src/assets/images/developer24.png";
-import dev26 from "/src/assets/images/developer26.png";
-import dev27 from "/src/assets/images/developer27.png";
-import dev28 from "/src/assets/images/developer28.png";
-import dev29 from "/src/assets/images/developer29.png";
-import dev30 from "/src/assets/images/developer30.png";
-import dev31 from "/src/assets/images/developer31.png";
-import dev25 from "/src/assets/images/developerx.png";
-
+import ellipse1 from '../assets/images/Ellipse 41.svg';
+import ellipse2 from '../assets/images/Ellipse 50.svg';
+import ellipse3 from '../assets/images/Ellipse 55.svg';
+import ellipse4 from '../assets/images/Ellipse 67.svg';
+import ellipse5 from '../assets/images/Ellipse 68.svg';
+import ellipse6 from '../assets/images/Ellipse 69.svg';
+import dev from '/src/assets/images/developer.png';
+import dev1 from '/src/assets/images/developer1.png';
+import dev2 from '/src/assets/images/developer2.png';
+import dev3 from '/src/assets/images/developer3.png';
+import dev4 from '/src/assets/images/developer4.png';
+import dev5 from '/src/assets/images/developer5.png';
+import dev6 from '/src/assets/images/developer6.png';
+import dev7 from '/src/assets/images/developer7.png';
+import dev8 from '/src/assets/images/developer8.png';
+import dev9 from '/src/assets/images/developer9.png';
+import dev10 from '/src/assets/images/developer10.png';
+import dev11 from '/src/assets/images/developer11.png';
+import dev12 from '/src/assets/images/developer12.png';
+import dev14 from '/src/assets/images/developery.png';
+import dev15 from '/src/assets/images/developer15.png';
+import dev16 from '/src/assets/images/developer16.png';
+import dev17 from '/src/assets/images/developer17.png';
+import dev18 from '/src/assets/images/developer18.png';
+import dev19 from '/src/assets/images/developer19.png';
+import dev20 from '/src/assets/images/developer20.png';
+import dev21 from '/src/assets/images/developer21.png';
+import dev22 from '/src/assets/images/developer22.png';
+import dev23 from '/src/assets/images/developer23.png';
+import dev24 from '/src/assets/images/developer24.png';
+import dev26 from '/src/assets/images/developer26.png';
+import dev27 from '/src/assets/images/developer27.png';
+import dev28 from '/src/assets/images/developer28.png';
+import dev29 from '/src/assets/images/developer29.png';
+import dev30 from '/src/assets/images/developer30.png';
+import dev31 from '/src/assets/images/developer31.png';
+import dev25 from '/src/assets/images/developerX.png';
 
 export const navLinks = [
   { id: 1, name: 'Home', url: '/' },
   { id: 2, name: 'Recruits', url: '/recruits' },
-  { id: 2, name: "Recruits", url: "/designers" },
-  { id: 3, name: "About Us", url: "/about" },
-  { id: 4, name: "Contact", url: "/contact" },
-  { id: 5, name: "Blog", url: "/blog" },
+  { id: 2, name: 'Recruits', url: '/designers' },
+  { id: 3, name: 'About Us', url: '/about' },
+  { id: 4, name: 'Contact', url: '/contact' },
+  { id: 5, name: 'Blog', url: '/blog' },
 ];
 
 import smile from '../assets/images/smile.png';
@@ -58,222 +57,222 @@ import manComp from '../assets/images/man-comp.png';
 import womanGarage from '../assets/images/woman-garage.png';
 
 export const loggedIn = [
-  { id: 1, name: "Message", url: "/" },
-  { id: 2, name: "Notification", url: "/about" },
-  { id: 3, name: "Profile", url: "/contact" },
-  { id: 4, name: "Logout", url: "/blog" },
+  { id: 1, name: 'Message', url: '/' },
+  { id: 2, name: 'Notification', url: '/about' },
+  { id: 3, name: 'Profile', url: '/contact' },
+  { id: 4, name: 'Logout', url: '/blog' },
 ];
 
 export const devTeam = [
   {
     id: 1,
-    Name: "Adebambo Jadesola",
+    Name: 'Adebambo Jadesola',
     experience: 1,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev16,
   },
   {
     id: 2,
-    Name: "Odunlami Oluwafemi",
+    Name: 'Odunlami Oluwafemi',
     experience: 1,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev,
   },
 
   {
     id: 3,
-    Name: "Musa Amoda",
+    Name: 'Musa Amoda',
     experience: 2,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev12,
   },
   {
     id: 4,
-    Name: "Mundung Weng",
+    Name: 'Mundung Weng',
     experience: 1,
-    Location: "Jalingo, Nigeria",
+    Location: 'Jalingo, Nigeria',
     Image: dev18,
   },
   {
     id: 5,
-    Name: "Obi Chimeze",
+    Name: 'Obi Chimeze',
     experience: 2,
-    Location: "Owerri, Nigeria",
+    Location: 'Owerri, Nigeria',
     Image: dev3,
   },
   {
     id: 6,
-    Name: "Aishat Muibudeen",
+    Name: 'Aishat Muibudeen',
     experience: 1,
-    Location: "Ibadan, Nigeria",
+    Location: 'Ibadan, Nigeria',
     Image: dev8,
   },
   {
     id: 7,
-    Name: "Daramola Adesewa",
+    Name: 'Daramola Adesewa',
     experience: 1,
-    Location: "Abuja, Nigeria",
+    Location: 'Abuja, Nigeria',
     Image: dev5,
   },
   {
     id: 8,
-    Name: "Isreal Clement",
+    Name: 'Isreal Clement',
     experience: 1,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev2,
   },
   {
     id: 9,
-    Name: "Bakare Shakiroh",
+    Name: 'Bakare Shakiroh',
     experience: 1,
-    Location: "Abuja, Nigeria",
+    Location: 'Abuja, Nigeria',
     Image: dev7,
   },
   {
     id: 10,
-    Name: "Adewole Michael",
+    Name: 'Adewole Michael',
     experience: 1,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev9,
   },
   {
     id: 11,
-    Name: "Kolade Oulwatosin",
+    Name: 'Kolade Oulwatosin',
     experience: 1,
-    Location: "Benin-city, Nigeria",
+    Location: 'Benin-city, Nigeria',
     Image: dev10,
   },
   {
     id: 12,
-    Name: "Garba Abigail",
+    Name: 'Garba Abigail',
     experience: 2,
-    Location: "Auchi, Nigeria",
+    Location: 'Auchi, Nigeria',
     Image: dev11,
   },
   {
     id: 13,
-    Name: "Egbe Ethan",
+    Name: 'Egbe Ethan',
     experience: 1,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev1,
   },
   {
     id: 14,
-    Name: "Soronnadi Lois",
+    Name: 'Soronnadi Lois',
     experience: 1,
-    Location: "Asaba, Nigeria",
+    Location: 'Asaba, Nigeria',
     Image: dev4,
   },
   {
     id: 15,
-    Name: "Fiiwe Barisanem",
+    Name: 'Fiiwe Barisanem',
     experience: 3,
-    Location: "PH-city, Nigeria ",
+    Location: 'PH-city, Nigeria ',
     Image: dev24,
   },
   {
     id: 16,
-    Name: "Amadi Augustina",
+    Name: 'Amadi Augustina',
     experience: 2,
-    Location: "Aba, Nigeria",
+    Location: 'Aba, Nigeria',
     Image: dev25,
   },
   {
     id: 17,
-    Name: "Abubakar Mistura",
+    Name: 'Abubakar Mistura',
     experience: 3,
-    Location: "Kano, Nigeria",
+    Location: 'Kano, Nigeria',
     Image: dev14,
   },
   {
     id: 18,
-    Name: "Ayodele Rafael",
+    Name: 'Ayodele Rafael',
     experience: 2,
-    Location: "Akure, Nigeria",
+    Location: 'Akure, Nigeria',
     Image: dev15,
   },
   {
     id: 19,
-    Name: "Atinyoh Imokha",
+    Name: 'Atinyoh Imokha',
     experience: 1,
-    Location: "Lafia, Nigeria",
+    Location: 'Lafia, Nigeria',
     Image: dev6,
   },
   {
     id: 20,
-    Name: "Charles Imoh",
+    Name: 'Charles Imoh',
     experience: 2,
-    Location: "Makurdi, Nigeria",
+    Location: 'Makurdi, Nigeria',
     Image: dev19,
   },
   {
     id: 21,
-    Name: "Park Lu Chen",
+    Name: 'Park Lu Chen',
     experience: 3,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev30,
   },
   {
     id: 19,
-    Name: "Idris Matilda",
+    Name: 'Idris Matilda',
     experience: 2,
-    Location: "Abeokuta, Nigeria",
+    Location: 'Abeokuta, Nigeria',
     Image: dev29,
   },
   {
     id: 20,
-    Name: "Coker Thompson",
+    Name: 'Coker Thompson',
     experience: 3,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev31,
   },
   {
     id: 21,
-    Name: "Okonkwo Kenneth",
+    Name: 'Okonkwo Kenneth',
     experience: 2,
-    Location: "Uyo, Nigeria",
+    Location: 'Uyo, Nigeria',
     Image: dev26,
   },
   {
     id: 22,
-    Name: "Bayero Funmilola",
+    Name: 'Bayero Funmilola',
     experience: 2,
-    Location: "Sokoto, Nigeria",
+    Location: 'Sokoto, Nigeria',
     Image: dev28,
   },
   {
     id: 23,
-    Name: "Adeleke Oyindamola",
+    Name: 'Adeleke Oyindamola',
     experience: 1,
-    Location: "Lagos, Nigeria",
+    Location: 'Lagos, Nigeria',
     Image: dev27,
   },
   {
     id: 21,
-    Name: "Shuaib Adebimpe",
+    Name: 'Shuaib Adebimpe',
     experience: 3,
-    Location: "Ilorin, Nigeria",
+    Location: 'Ilorin, Nigeria',
     Image: dev20,
   },
   {
     id: 22,
-    Name: "Abdullahi Danlad",
+    Name: 'Abdullahi Danlad',
     experience: 3,
-    Location: "Zaria, Kaduna",
+    Location: 'Zaria, Kaduna',
     Image: dev22,
   },
   {
     id: 23,
-    Name: "Moses Trabaye",
+    Name: 'Moses Trabaye',
     experience: 1,
-    Location: "Bauchi, Nigeria",
+    Location: 'Bauchi, Nigeria',
     Image: dev21,
   },
   {
     id: 23,
-    Name: "Nuel Adepoju",
+    Name: 'Nuel Adepoju',
     experience: 2,
-    Location: "Ijebu-ode, Nigeria",
+    Location: 'Ijebu-ode, Nigeria',
     Image: dev23,
   },
 ];
@@ -281,28 +280,35 @@ export const devTeam = [
 export const tabTitles = [
   {
     id: 1,
-    title: "Personal Details",
+    title: 'Personal Details',
   },
   {
     id: 2,
-    title: "Experience",
+    title: 'Experience',
   },
   {
     id: 3,
-    title: "Personal Reviews",
+    title: 'Personal Reviews',
   },
 ];
 
 export const topRecruits = [
   { id: 1, name: 'Kolade Maya', track: 'Backend Developer', img: ellipse3 },
   {
-    id: 2, name:
-      'Odunlami Oluwafemi', track: 'UI/UX', img: ellipse1
+    id: 2,
+    name: 'Odunlami Oluwafemi',
+    track: 'UI/UX',
+    img: ellipse1,
   },
   { id: 3, name: 'Bakare Aishat', track: 'Frontend Developer', img: ellipse2 },
   { id: 4, name: 'Temitope Fabian', track: 'CAD', img: ellipse4 },
-  { id: 5, name: 'Daramola Adesanya', track: 'Product Management', img: ellipse5 },
-  { id: 6, name: 'Orji Clement', track: 'CAD', img: ellipse6 }
+  {
+    id: 5,
+    name: 'Daramola Adesanya',
+    track: 'Product Management',
+    img: ellipse5,
+  },
+  { id: 6, name: 'Orji Clement', track: 'CAD', img: ellipse6 },
 ];
 
 export const DesignersInfo = [
