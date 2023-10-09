24ed6e5a654dbdade9fc5390042140b57c48d96d
odomfavour <ogochukwuodom@gmail.com>
2023-10-09 11:49:38 +0100

    ch: added add to cart

--- src/pages/Developers.jsx
+++ src/pages/Developers.jsx
@@ -1,113 +1,111 @@
+import LoggedInNavbar from '../components/LoggedInNavbar';
+import rect from '../assets/Images/Rect.png';
+import star from '../assets/Icons/star-light.png';
+import star2 from '../assets/Icons/star-grey.png';
+import timer from '../assets/Icons/timer.png';
+import symbols from '../assets/Icons/symbols.png';
+import locator from '../assets/Icons/locator.png';
 
-import LoggedInNavbar from "../components/LoggedInNavbar";
-import rect from "/src/assets/Images/Rect.png"
-import star from "/src/assets/Icons/star-light.png";
-import star2 from "/src/assets/Icons/star-grey.png";
-import timer from "/src/assets/Icons/timer.png";
-import symbols from "/src/assets/Icons/symbols.png";
-import locator from "/src/assets/Icons/locator.png";
-
-import { devTeam } from "../utils/data";
-
+import { devTeam } from '../utils/data';
 
 const Developers = () => {
+  return (
+    <section>
+      <LoggedInNavbar />
 
+      <div className=' w-[70%] container mx-auto'>
+        <button className='w-[178px] h-[48px] text-[#FFFFFF] text-[16px] font-bold  bg-[#2776EA] mt-[90px] rounded-t-md'>
+          Find a Candidate
+        </button>
+        <div className='bg-[#F5F5F5] p-4  flex flex-col md:flex-row  flex-wrap  items-center   gap-5 md:gap-11 '>
+          <div className='relative'>
+            <div className='absolute top-3'>
+              <img src={timer} />
+            </div>
+            <select className='px-6 py-4 w-[210px]'>
+              <option className='m-auto'>Product Developer</option>
+            </select>
+          </div>
 
-    return (
-        <section>
-            <LoggedInNavbar />
+          <div className='relative'>
+            <div className='absolute top-3 left-2'>
+              <img src={symbols} />
+            </div>
+            <select className='px-11 py-4  w-[210px]'>
+              <option className='p-9'>All</option>
+            </select>
+          </div>
 
-            <div className=" w-[70%] container mx-auto">
-                <button className="w-[178px] h-[48px] text-[#FFFFFF] text-[16px] font-bold  bg-[#2776EA] mt-[90px] rounded-t-md">Find a Candidate</button>
-                <div className="bg-[#F5F5F5] p-4  flex flex-col md:flex-row  flex-wrap  items-center   gap-5 md:gap-11 ">
+          <div className='relative'>
+            <div className='absolute top-3 left-2'>
+              <img src={locator} />
+            </div>
+            <select className='px-7  py-4  w-[210px]'>
+              <option className='p-9'>Nigeria</option>
+            </select>
+          </div>
 
-                    <div className="relative">
-                        <div className="absolute top-3">
-                            <img src={timer} />
-                        </div>
-                        <select className="px-6 py-4 w-[210px]">
-                            <option className="m-auto">
-                                Product Developer</option>
+          <button className='bg-[#2776EA] w-[210px] h-[46px]   md:ml-[60px]  text-[#FFFFFF]'>
+            Search
+          </button>
+        </div>
+      </div>
+      <div className='flex w-[80%] container mx-auto mt-7 gap-4'>
+        <img className='w-[10px] h-[48px] mt-3' src={rect} />
+        <h2 className=' text-[24px] md:text-[48px] mt-4 md:mt-0 font-bold'>
+          Software Developers
+        </h2>
+      </div>
 
-                        </select>
-                    </div>
+      <div className='flex flex-wrap  justify-between gap-y-11 lg:gap-[170px] lg:w-[70%] w-[80%] container mx-auto mt-9'>
+        {devTeam.map((developers) => {
+          const { id, Image, NAME, experience, LOCATION } = developers;
 
-                    <div className="relative">
-                        <div className="absolute top-3 left-2">
-                            <img src={symbols} />
-                        </div>
-                        <select className="px-11 py-4  w-[210px]">
-                            <option className="p-9">
-                                All</option>
-
-                        </select>
-                    </div>
-
-                    <div className="relative">
-                        <div className="absolute top-3 left-2" >
-                            <img src={locator} />
-                        </div>
-                        <select className="px-7  py-4  w-[210px]">
-                            <option className="p-9">
-                                Nigeria</option>
-
-                        </select>
-                    </div>
-
-
-                    <button className="bg-[#2776EA] w-[210px] h-[46px]   md:ml-[60px]  text-[#FFFFFF]">Search</button>
-
+          return (
+            <div key={id} className='w-[250px]'>
+              <div>
+                <img src={Image} />
+              </div>
+              <div className='w-[250px] mt-5 '>
+                <h3 className='text-[16px] font-bold text-[#00000] '>
+                  NAME: <span className=' text-[#2776EA]'>{NAME}</span>
+                </h3>
+                <h3 className='text-[16px] font-bold text-[#00000] '>
+                  YEARS OF EXPERIENCE:{' '}
+                  <span className=' text-[#2776EA]'>{experience}</span>
+                </h3>
+                <h3 className='text-[16px] font-bold text-[#00000] '>
+                  STRENGTH: <span className=' text-[#2776EA]'>FRONT-END</span>
+                </h3>
+                <h3 className='text-[16px] font-bold text-[#00000] '>
+                  LOCATION: <span className=' text-[#2776EA]'>{LOCATION}</span>
+                </h3>
+              </div>
+              <div className='flex m-auto gap-3 mt-3'>
+                <div className='flex'>
+                  <img className='w-[20px] h-[20px]' src={star} />
+                  <img className='w-[20px] h-[20px]' src={star} />
+                  <img className='w-[20px] h-[20px]' src={star} />
+                  <img className='w-[20px] h-[20px]' src={star} />
+                  <img className='w-[20px] h-[20px]' src={star2} />
                 </div>
+                <div>
+                  <p>[11]</p>
+                </div>
+              </div>
 
+              <button className='w-[144px] bg-[#2776EA] h-[45px] mt-3 text-[#FFFF] font-bold text-[24px]'>
+                View
+              </button>
             </div>
-            <div className="flex w-[80%] container mx-auto mt-7 gap-4">
-                <img className="w-[10px] h-[48px] mt-3" src={rect} />
-                <h2 className=" text-[24px] md:text-[48px] mt-4 md:mt-0 font-bold">Software  Developers</h2>
-            </div>
+          );
+        })}
+      </div>
 
-            <div className="flex flex-wrap  justify-between gap-y-11 lg:gap-[170px] lg:w-[70%] w-[80%] container mx-auto mt-9">
-                {devTeam.map((developers) => {
-
-                    const { id, Image, NAME, experience, LOCATION } = developers
-
-
-                    return (
-                        <div key={id} className="w-[250px]">
-                            <div>
-                                <img src={Image} />
-                            </div>
-                            <div className="w-[250px] mt-5 ">
-                                <h3 className="text-[16px] font-bold text-[#00000] ">NAME: <span className=" text-[#2776EA]">{NAME}</span></h3>
-                                <h3 className="text-[16px] font-bold text-[#00000] ">YEARS OF EXPERIENCE:  <span className=" text-[#2776EA]">{experience}</span></h3>
-                                <h3 className="text-[16px] font-bold text-[#00000] ">STRENGTH:  <span className=" text-[#2776EA]">FRONT-END</span></h3>
-                                <h3 className="text-[16px] font-bold text-[#00000] ">LOCATION:  <span className=" text-[#2776EA]">{LOCATION}</span></h3>
-
-                            </div>
-                            <div className="flex m-auto gap-3 mt-3" >
-                                <div className="flex">
-                                    <img className="w-[20px] h-[20px]" src={star} />
-                                    <img className="w-[20px] h-[20px]" src={star} />
-                                    <img className="w-[20px] h-[20px]" src={star} />
-                                    <img className="w-[20px] h-[20px]" src={star} />
-                                    <img className="w-[20px] h-[20px]" src={star2} />
-                                </div>
-                                <div>
-                                    <p>[11]</p>
-                                </div>
-
-                            </div>
-
-                            <button className="w-[144px] bg-[#2776EA] h-[45px] mt-3 text-[#FFFF] font-bold text-[24px]">View</button>
-
-                        </div>
-                    )
-                })}
-            </div>
-
-            <button className="w-[227px] h-[64px]  py-5 justify-center border-[#2776EA] border-2 flex m-auto mt-[90px] mb-7">See More</button>
-
-
-        </section>
-    )
-}
+      <button className='w-[227px] h-[64px]  py-5 justify-center border-[#2776EA] border-2 flex m-auto mt-[90px] mb-7'>
+        See More
+      </button>
+    </section>
+  );
+};
 export default Developers;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,11 +6,13 @@ import { Link } from 'react-router-dom';
 const Products = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
 
   const getProducts = async () => {
     try {
       setIsLoading(true);
       const response = await axios.get('/products');
+      console.log(response);
       if (response.status === 200) {
         setProducts(response.data);
         setIsLoading(false);
@@ -18,6 +20,17 @@ const Products = () => {
     } catch (error) {
       console.log(error);
     }
+  };
+
+  const addItem = (item) => {
+    console.log('item', item);
+    let isINCart = cart.some((prod) => prod.id === item.id);
+    if (!isINCart) {
+      setCart([...cart, item]);
+    }
+    // console.log(updatedCart);
+
+    // setCart([...cart, item]);
   };
 
   useEffect(() => {
@@ -29,42 +42,114 @@ const Products = () => {
       {isLoading ? (
         <p className='text-center'>Loading</p>
       ) : (
-        <div className='grid grid-cols-4 gap-5'>
-          {products.length > 0 &&
-            products.map((product) => {
-              const { id, title, price, description, category, image, rating } =
-                product;
-
-              return (
-                <Link
-                  to={`/products/${id}`}
-                  key={id}
-                  className=' border border-orange-400 rounded-md p-4'
-                >
-                  <img
-                    src={image}
-                    alt={description}
-                    className='h-40  w-full object-scale-down'
-                  />
-                  <div>
-                    <p>{title}</p>
-                    <p>{description}</p>
-                    <p>{category}</p>
-                    <p>${price}</p>
-                    <p>{rating.rate}</p>
-                    <p className='flex'>
-                      {[...Array(Math.round(rating.rate))].map((e, i) => (
-                        <BsStarFill key={i} className=' text-yellow-600' />
-                      ))}
-                      {[...Array(5 - Math.round(rating.rate))].map((e, i) => (
-                        <BsStar key={i} className='text-yellow-600' />
-                      ))}
-                    </p>
+        <section>
+          <p>Items in cart: {cart.length}</p>
+          <div className='grid grid-cols-4 gap-5'>
+            {cart.length > 0 &&
+              cart.map((product) => {
+                const {
+                  id,
+                  title,
+                  price,
+                  description,
+                  category,
+                  image,
+                  rating,
+                } = product;
+                return (
+                  <div
+                    key={id}
+                    className=' border border-orange-400 rounded-md p-4'
+                  >
+                    <img
+                      src={image}
+                      alt={description}
+                      className='h-40  w-full object-scale-down'
+                    />
+                    <div>
+                      <p>{title}</p>
+                      <p>{description}</p>
+                      <p>{category}</p>
+                      <p>${price}</p>
+                      <p>{rating.rate}</p>
+                      <p className='flex'>
+                        {[...Array(Math.round(rating.rate))].map((e, i) => (
+                          <BsStarFill key={i} className=' text-yellow-600' />
+                        ))}
+                        {[...Array(5 - Math.round(rating.rate))].map((e, i) => (
+                          <BsStar key={i} className='text-yellow-600' />
+                        ))}
+                      </p>
+                      <div className='flex justify-between my-6'>
+                        <button
+                          onClick={() => addItem(product)}
+                          className='py-3 px-5 rounded-md bg-blue-600 text-white'
+                        >
+                          Add to cart
+                        </button>
+                        <Link to={`/products/${id}`} className='text-blue-500'>
+                          See Product
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                </Link>
-              );
-            })}
-        </div>
+                );
+              })}
+          </div>
+          <hr />
+          <div className='grid grid-cols-4 gap-5'>
+            {products.length > 0 &&
+              products.map((product) => {
+                const {
+                  id,
+                  title,
+                  price,
+                  description,
+                  category,
+                  image,
+                  rating,
+                } = product;
+                return (
+                  <div
+                    key={id}
+                    className=' border border-orange-400 rounded-md p-4'
+                  >
+                    <img
+                      src={image}
+                      alt={description}
+                      className='h-40  w-full object-scale-down'
+                    />
+                    <div>
+                      <p>{title}</p>
+                      <p>{description}</p>
+                      <p>{category}</p>
+                      <p>${price}</p>
+                      <p>{rating.rate}</p>
+                      <p className='flex'>
+                        {[...Array(Math.round(rating.rate))].map((e, i) => (
+                          <BsStarFill key={i} className=' text-yellow-600' />
+                        ))}
+                        {[...Array(5 - Math.round(rating.rate))].map((e, i) => (
+                          <BsStar key={i} className='text-yellow-600' />
+                        ))}
+                      </p>
+                      <div className='flex justify-between my-6'>
+                        <button
+                          onClick={() => addItem(product)}
+                          className='py-3 px-5 rounded-md bg-blue-600 text-white'
+                        >
+                          Add to cart
+                        </button>
+                        <Link to={`/products/${id}`} className='text-blue-500'>
+                          See Product
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </section>
       )}
     </div>
   );
